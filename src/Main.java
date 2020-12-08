4cf6527300a79a57ff13c34b91985fbf34d27d4d
KristinaTrahtman <kristina.trahtman@gmail.com>
2020-12-08 23:01:01 +0200

    Random letters function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Your Chosen Letter Is.......:");
+        //System.out.println("Value of char A is: " + Integer.valueOf('d'));
+        System.out.println("Your Chosen Letter Is.......:" + RandomLetter.randomLetter());
     }
 
     }
