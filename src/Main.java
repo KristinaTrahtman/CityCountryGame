6cfd9d16c7ee95dbd7f34c4c94166a638d7c3263
KristinaTrahtman <kristina.trahtman@gmail.com>
2020-12-08 22:00:02 +0200

    random letter

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
-    public static void main(String [] args ){
-    System.out.println("ok lets move on");
+import java.util.Random;
 
+// Example - Java class to generate random characters
+public class Main {
+    public static final String ALPHANUMERIC_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static void main(String[] args) {
+        System.out.println("Random Alphabet:"+getRandomAlphabet());
     }
+
+    // Create a random alphabet in Java
+    // Only lowercase letters
+    private static String getRandomAlphabet() {
+        Random r = new Random();
+        return String.valueOf((char)(r.nextInt(26)+'a'));
+    }
+
+
 }
