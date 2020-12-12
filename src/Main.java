e7e90c79b3b845cd262da51fb83819d8bc8cb251
KristinaTrahtman <kristina.trahtman@gmail.com>
2020-12-12 16:52:26 +0200

    React addition

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Value of char A is: " + Integer.valueOf('d'));
-        System.out.println("Your Chosen Letter Is.......:" + RandomLetter.randomLetter());
+        System.out.println("Your Chosen Letter Is......." + RandomLetter.randomLetter());
     }
 
     }
