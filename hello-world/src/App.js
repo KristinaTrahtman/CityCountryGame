e9b1cab9f4ddc255efd8c1b172e5321eeda04070
KristinaTrahtman <kristina.trahtman@gmail.com>
2020-12-15 19:08:59 +0200

    React timer

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Greet from './components/Greet'
 import GameTitle from './components/GameTitle'
 import Country from './components/Country'
+import Timer from './components/Timer'
 // import Message from './components/Message'
 
 
 function App() {
   return (
     <div className="App">
+      <Timer />
       <GameTitle />
 <Country/>
     {/*<Greet /> */} 
