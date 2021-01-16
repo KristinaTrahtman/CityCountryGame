776da84972551d4cca3bc8ae7ab6a0661396ab93
KristinaTrahtman <kristina.trahtman@gmail.com>
2021-01-16 21:17:50 +0200

    Clean up version 2

--- test/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //System.out.println("Value of char A is: " + Integer.valueOf('d'));
-        System.out.println("Your Chosen Letter Is......." + RandomLetter.randomLetter());
-    }
-
-    }
--- test/src/RandomLetter.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-public class RandomLetter {
-    public static String randomLetter() {
-        Random r = new Random();
-        return String.valueOf((char)(r.nextInt(26)+'A'));
-    }
-}
-
-
