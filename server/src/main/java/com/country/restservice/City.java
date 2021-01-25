68d3f7dffe9f4788ebb0194c03fe844312d62193
Paz Zchut <53433538+pazchut@users.noreply.github.com>
2021-01-25 23:40:33 +0200

    Update City.java

--- server/src/main/java/com/country/restservice/City.java
+++ server/src/main/java/com/country/restservice/City.java
@@ -1,4 +1,5 @@
 package com.country.restservice;
+//P.Z: I think I would prefer to move all those files to their own "categories" dir or something.
 public class City extends CategoryFromJson {
     public City(String userCategoryAnswer) {
         this.categoryName = "City";
