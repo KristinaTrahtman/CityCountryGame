0a73cf94ff643953ec923b7955b79cd6df3b15ff
Paz Zchut <53433538+pazchut@users.noreply.github.com>
2021-01-25 23:43:10 +0200

    Update GameTitle.js

--- ui/src/components/GameTitle.js
+++ ui/src/components/GameTitle.js
@@ -4,6 +4,7 @@ import './myStyles.css'
 const GameTitle = () => {
     return (
         <div className = "GameTitle">
+        // P.Z: Try to make yourself a habbit and use Auto-formatting more often.
         <h1 className = 'primary' >City Country Game</ h1>
         </div>
     )
