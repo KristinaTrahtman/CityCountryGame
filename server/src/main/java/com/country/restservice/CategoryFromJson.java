5846576628cc3f7620bd42a297e29b4ed6a0014f
Paz Zchut <53433538+pazchut@users.noreply.github.com>
2021-01-25 23:39:18 +0200

    Update CategoryFromJson.java

--- server/src/main/java/com/country/restservice/CategoryFromJson.java
+++ server/src/main/java/com/country/restservice/CategoryFromJson.java
@@ -15,7 +15,9 @@ public class CategoryFromJson extends Category {
         JSONParser parser = new JSONParser();
         try {
             JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(jsonFilePath));
-
+            // P.Z: Usually redundant blank lines are a sign for an overloaded function.
+            // It's a good intuition to saparate and orgenize your code, but try to do it with extracting code to functions instead of using blank lines.
+            
             Iterator<JSONObject> iterator = jsonArray.iterator();
             while (iterator.hasNext()) {
                 if (userCategoryAnswer.equalsIgnoreCase((String) iterator.next().get("name"))) {
