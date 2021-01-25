35b44ad38933f9a670676bcdc4208f70ee39f706
Paz Zchut <53433538+pazchut@users.noreply.github.com>
2021-01-25 23:35:00 +0200

    Update Actor.java

--- server/src/main/java/com/country/restservice/Actor.java
+++ server/src/main/java/com/country/restservice/Actor.java
@@ -1,8 +1,11 @@
 package com.country.restservice;
 
+//P.Z: WOW! such a different. so much more beautiful!
+
 public class Actor extends CategoryFromJson{
     public Actor(String userCategoryAnswer) {
         this.categoryName = "Actor";
+        // P.Z: please use categoryName to generate this path and move it to CategoryFromJson
         this.jsonFilePath = "src/main/java/categoriesDataJson/actors.json";
         this.userCategoryAnswer = userCategoryAnswer;
     }
